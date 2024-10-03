8631b00532c6ad2f426ae602d98634b885ae8791
gabrielsant13 <79643471+gabrielsant13@users.noreply.github.com>
2024-10-03 09:03:46 -0300

    Primeiro commit app.py

---
+++ app.py
@@ -0,0 +1,67 @@
+import os
+import cx_Oracle
+import time
+import datetime
+
+#caminho de diretorio local
+cx_Oracle.init_oracle_client(lib_dir=r"C:\app\client\product\12.2.0\client_1\bin")
+
+# Configuração do banco de dados Oracle
+dsn = cx_Oracle.makedsn("ip", "porta", service_name="service_name")
+user = "user"
+password = "senha"
+
+# Configuração de caminhos das pastas
+caminhoDestino = str("C:\\Users\\joao.santiago\\Desktop\\python\\Integracao_img_1203\\destino\\")
+caminhoOrigem = str("C:\\Users\\joao.santiago\\Desktop\\python\\Integracao_img_1203\\origem\\")
+
+#funcao de tempo
+def sysdate():
+    ano,mes,dia,hora,min,seg,diaSemana,diaAno, horarioVerao= time.localtime()
+    #return(f'{dia}/{mes}/{ano} {hora}:{min}:{seg} diaSemana:{diaSemana} diaAno:{diaAno}')
+    return (f'{'{:02d}'.format(dia)} {'{:02d}'.format(mes)} {ano} {'{:02d}'.format(hora)} {'{:02d}'.format(min)} {'{:02d}'.format(seg)} {diaSemana} {diaAno}')
+
+# Função utilizada para captar dias da semana
+def datahora():
+    tempo = sysdate().split()
+    datahora = datetime.datetime.strptime(f'{tempo[0]}/{tempo[1]}/{tempo[2]} {tempo[3]}:{tempo[4]}:{tempo[5]}', "%d/%m/%Y %H:%M:%S")
+    return datahora
+
+# lógica
+print('validação da logica\n')
+while 1:
+    while sysdate().split()[2] < '2027': # Trava de tempo
+        tempo = sysdate().split()
+        
+        # Valida se o tempo está de acordo com os valores abaixo, se estiver inicia os processos
+        if tempo[4] == '00' and tempo[5] <= '01':
+            
+            # Verifica o nome das pastas na Ion e salva em variável para validação
+            clientes = [pasta for pasta in os.listdir(caminhoOrigem) if os.path.isdir(os.path.join(caminhoOrigem, pasta))]
+
+            conn = cx_Oracle.connect(user=user, password=password, dsn=dsn)
+            cursor = conn.cursor()
+
+            for cliente in clientes:
+                try:
+                    cursor.execute(f"select codcli,numeros(cgcent) cgcent from pcclient where numeros(cgcent) in ('{cliente}')")
+                    valor = cursor.fetchall()
+
+                    codcli = str(valor[0][0])
+                    cgcent = str(valor[0][1])
+
+                    # Renomeia e altera a pasta
+                    os.renames(caminhoOrigem+cgcent, caminhoDestino+'00'+codcli)
+
+                    # Salva no log
+                    arquivo = open(caminhoOrigem+'log.txt',"a")
+                    arquivo.write(f'Importado cgcent: {cliente}, codcli: {codcli} - {datahora()}\n')
+                    arquivo.close()
+
+                    print(f'Ok: {cliente} - {codcli}')
+                except:
+                    print(f'Cliente não cadastrado ou não existe: {cliente}')
+                    
+
+            cursor.close()
+            conn.close()
