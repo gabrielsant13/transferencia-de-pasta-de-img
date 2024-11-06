cabb6ee43dee5338b0154fdb5b6752316e76dfdc
João Santiago <79643471+gabrielsant13@users.noreply.github.com>
2024-11-06 15:18:25 -0300

    Ajuste para redução de processamento de cpu
    foi alterada a forma como o programa espera, para realização da próxima consulta, linha 72

--- app.py
+++ app.py
@@ -6,15 +6,15 @@ import datetime
 os.system("cls")
 
 #caminho de diretorio local
-# cx_Oracle.init_oracle_client(lib_dir=r"C:\app\client\product\12.2.0\client_1\bin")
+cx_Oracle.init_oracle_client(lib_dir=r"C:\app\client\product\12.2.0\client_1\bin")
 
 # Configuração do banco de dados Oracle
-dsn = cx_Oracle.makedsn("ip", "port", service_name="service_name")
+dsn = cx_Oracle.makedsn("ip", "porta", service_name="service_name")
 user = "user"
-password = "password"
+password = "password?"
 
 # Configuração de caminho da pasta
-caminho = str("C:\\ION Sistemas\\CLIENTES\\")
+caminho = str("C:\\ION Sistemas\\clientes\\")
 
 #funcao de tempo
 def sysdate():
@@ -30,42 +30,43 @@ def datahora():
 
 # lógica
 print('validação da logica\n')
-while 1:
-    while sysdate().split()[2] < '2027': # Trava de tempo
-        tempo = sysdate().split()
+while sysdate().split()[2] < '2027': # Trava de tempo
+
+    # Verifica o nome das pastas na Ion e salva em variável para validação
+    clientes = [pasta for pasta in os.listdir(caminho) if os.path.isdir(os.path.join(caminho, pasta))]
+
+    conn = cx_Oracle.connect(user=user, password=password, dsn=dsn)
+    cursor = conn.cursor()
+
+    for cliente in clientes:
+        try:
+            cursor.execute(f"select codcli,numeros(cgcent) cgcent from pcclient where numeros(cgcent) in ('{cliente}')")
+            valor = cursor.fetchall()
+
+            codcli = str(valor[0][0])
+            cgcent = str(valor[0][1])
+
+            # Renomeia e altera a pasta
+            os.renames(caminho+cgcent, caminho+'00'+codcli)
+
+            # Salva no log
+            arquivo = open(caminho+'log.txt',"a")
+            arquivo.write(f'Importado cgcent: {cliente}, codcli: {codcli} - {datahora()}\n')
+            arquivo.close()
+
+            print(f'Ok: {cliente} - {codcli}')
+        except Exception as e:
+            # Salva no log
+            arquivo = open(caminho+'log.txt',"a")
+            arquivo.write(f'Cliente não cadastrado ou não existe: {cliente} | {e} - {datahora()}\n')
+            arquivo.close()
+
+            print(f'Cliente não cadastrado ou não existe: {cliente} | {e} - {datahora()}')
         
-        # Valida se o tempo está de acordo com os valores abaixo, se estiver inicia os processos
-        if tempo[4] == '57' and tempo[5] <= '01':
-            
-            # Verifica o nome das pastas na Ion e salva em variável para validação
-            clientes = [pasta for pasta in os.listdir(caminho) if os.path.isdir(os.path.join(caminho, pasta))]
+    print('------------------------------------------------------------------')
 
-            conn = cx_Oracle.connect(user=user, password=password, dsn=dsn)
-            cursor = conn.cursor()
+    cursor.close()
+    conn.close()
 
-            for cliente in clientes:
-                try:
-                    cursor.execute(f"select codcli,numeros(cgcent) cgcent from pcclient where numeros(cgcent) in ('{cliente}')")
-                    valor = cursor.fetchall()
-
-                    codcli = str(valor[0][0])
-                    cgcent = str(valor[0][1])
-
-                    # Renomeia e altera a pasta
-                    os.renames(caminho+cgcent, caminho+'00'+codcli)
-
-                    # Salva no log
-                    arquivo = open(caminho+'log.txt',"a")
-                    arquivo.write(f'Importado cgcent: {cliente}, codcli: {codcli} - {datahora()}\n')
-                    arquivo.close()
-
-                    print(f'Ok: {cliente} - {codcli}')
-                except:
-                    print(f'Cliente não cadastrado ou não existe: {cliente}')
-                
-            print('------------------------------------------------------------------')
-
-                    
-
-            cursor.close()
-            conn.close()
+    # Execução do processo a cada 1h, a partir da inicialização do serviço
+    time.sleep(3600)
